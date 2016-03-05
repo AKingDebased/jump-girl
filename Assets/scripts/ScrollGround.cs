2ddea0df2d0112681279ba8f9b3e408423dc4213
gabriel <ggonzalv@lion.lmu.edu>
2016-03-05 13:22:15 -0800

    FINALLY fix scrolling ground
    switched to a single scrolling texture

---
+++ Assets/scripts/ScrollGround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollGround : MonoBehaviour {
+
+	public float speed = 5f;
+	
+	private Renderer renderer;
+
+	void Start(){
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update () {
+		Vector2 offset = new Vector2(Time.deltaTime / speed,0);
+
+		renderer.material.mainTextureOffset += offset;
+
+	}
+}
